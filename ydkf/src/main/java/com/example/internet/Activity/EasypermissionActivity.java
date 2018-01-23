781a5a7a6db8aebb2dec5fb63419cc06269c261b
wangrui <wangrui@example.com>
2018-01-23 17:01:03 +0800

    第一次

---
+++ ydkf/src/main/java/com/example/internet/Activity/EasypermissionActivity.java
@@ -0,0 +1,34 @@
+package com.example.internet.Activity;
+
+import android.view.View;
+
+import com.example.internet.R;
+
+public class EasypermissionActivity extends BaseActivity {
+
+
+    @Override
+    public void setOnClick() {
+
+    }
+
+    @Override
+    public int initLayout() {
+        return R.layout.activity_easypermission;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void widgetClick(View v) {
+
+    }
+}
